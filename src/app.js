e5b31430438536c4d3b21d7b9ca283f9729218e0
skirt-owner <skirtsfield@gmail.com>
2023-12-18 19:42:50 +0000

    Inital commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const serverPort = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(serverPort, () => {
+    console.log(`Server is running on port ${serverPort}`);
+});
