41189c9efa57ca24416b824b101462c7ae3de79f
Mac-Auto <makar_kochergin@mail.ru>
2023-12-19 19:54:19 +0000

    changed app

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const serverPort = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.listen(serverPort, () => {
     console.log(`Server is running on port ${serverPort}`);
 });
